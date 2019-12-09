edc7d72bbdac2ca6b66098247611edd5bff4501d
rianders <rianders@docs.rutgers.edu>
2019-12-09 09:59:08 -0500

    add the showH5 code

---
+++ showH5.py
@@ -0,0 +1,12 @@
+from skimage import data, color, io
+from skimage.transform import resize
+import datetime as dt
+import h5py
+import matplotlib.pyplot as plt
+import matplotlib.pylab as plb
+import numpy as np
+import os
+import pandas as pd
+from glob import glob
+import argparse
+
